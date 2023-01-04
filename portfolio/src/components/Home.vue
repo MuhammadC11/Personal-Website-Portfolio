e0371b01c0fc6c06ca8ad906f67e9c056e3ed492
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2023-01-03 19:12:04 -0500

    pocoloco flow

--- portfolio/src/components/Home.vue
+++ portfolio/src/components/Home.vue
@@ -1,68 +1,23 @@
 <template>
-  <div>
-    <svg
-      class="background--custom"
-      id="demo"
-      viewBox="0 0 100 100"
-      preserveAspectRatio="none"
-    >
-      <path
-        fill="#8ac5ee"
-        fill-opacity="0.4"
-        d="M-100 -100L200 -100L200 50L-100 50Z"
-        style="animation: path0 3.4246575342465753s linear infinite alternate"
-      />
-      <path
-        fill="#f5261f"
-        fill-opacity="0.1"
-        d="M-100 -100L200 -100L200 80L-100 80Z"
-        style="animation: path1 62.5s linear infinite alternate"
-      />
-      <path
-        fill="#6e4973"
-        fill-opacity="0.2"
-        d="M-100 -100L200 -100L200 90L-100 90Z"
-        style="animation: path2 26.31578947368421s linear infinite alternate"
-      />
-    </svg>
-  </div>
+  <div class="background--custom"></div>
 </template>
-
 <style>
-path {
-  transform-origin: 50% 0%;
-}
 .background--custom {
-  background-color: #ffffff;
+  background: linear-gradient(90deg, #114df0, #030303, #f00300);
+  background-size: 300% 300%;
+  animation: gradient 8s alternate infinite;
   position: absolute;
-  width: 100vw;
-  height: 100vh;
-  z-index: -1;
   top: 0;
   left: 0;
+  height: 100%;
+  width: 100%;
 }
-@keyframes path0 {
+@keyframes gradient {
   0% {
-    transform: rotate(-20deg);
+    background-position: 0%;
   }
   100% {
-    transform: rotate(20deg);
-  }
-}
-@keyframes path1 {
-  0% {
-    transform: rotate(-20deg);
-  }
-  100% {
-    transform: rotate(20deg);
-  }
-}
-@keyframes path2 {
-  0% {
-    transform: rotate(50deg);
-  }
-  100% {
-    transform: rotate(-50deg);
+    background-position: 100%;
   }
 }
 </style>
