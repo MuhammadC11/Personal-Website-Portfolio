c4875da45d6244e5ad8d71db072bf56e8414af5c
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2023-01-04 00:35:04 -0500

    bringing over NavBar from Vanta branch

--- portfolio/src/components/Navbar.vue
+++ portfolio/src/components/Navbar.vue
@@ -1,30 +1,16 @@
 <template>
-  <!-- create a navbar with options for projects, and resume -->
-  <div class="container">
-    <router-link to="/" aria-label="link to the homepage">
-      <button class="button-1" type="button" name="button" aria-label="home">
-        <i class="fa fa-home"></i></button
-    ></router-link>
-    <!-- <router-link to="/births"
-      ><button class="button-1" type="button" name="button" aria-label="births">
-        Births
-      </button>
-    </router-link>
-    <router-link to="/deaths"
-      ><button class="button-1" type="button" name="button" aria-label="deaths">
-        Deaths
-      </button>
-    </router-link> -->
+  <div class="navbar_container">
+    <div class="home_button">
+      <router-link to="/" aria-label="link to the homepage">
+        <button class="button1">Muhammad Chaudhry</button>
+      </router-link>
+    </div>
+    <div class="resume_button">
+      <router-link to="/resume" aria-label="link to resume">
+        <button class="button1">Résumé</button>
+      </router-link>
+    </div>
   </div>
-  <!-- create a section for each project -->
-  <!-- create a section for resume -->
-  <!-- create a section for contact -->
-  <!-- create a footer with links to github, linkedin, and email -->
-  <!-- create a section for about me -->
-  <!-- create a section for skills -->
-  <!-- create a section for education -->
-  <!-- create a section for experience -->
-  <!-- create a section for projects -->
 </template>
 
 <script setup></script>
@@ -33,19 +19,64 @@
 @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
 @import "https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap";
 
-.container {
-  text-align: center;
-  padding: 1rem;
-  width: 100%;
-  background-color: #e1c9d1;
+.button1 {
+  font-size: 18px;
+  color: #ffffff;
+  font-family: inherit;
+  font-weight: 800;
+  cursor: pointer;
+  position: relative;
+  border: none;
+  background: none;
+  text-transform: uppercase;
+  transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+  transition-duration: 400ms;
+  transition-property: color;
 }
-.button-1 {
-  appearance: none;
-  background-color: #e1e5ed;
-  border: 2px solid #2a324b;
+
+.button1:focus,
+.button1:hover {
+  color: #fff;
+}
+
+.button1:focus:after,
+.button1:hover:after {
+  width: 100%;
+  left: 0%;
+}
+
+.button1:after {
+  content: "";
+  pointer-events: none;
+  bottom: -2px;
+  left: 50%;
+  position: absolute;
+  width: 0%;
+  height: 2px;
+  background-color: #fff;
+  transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+  transition-duration: 400ms;
+  transition-property: width, left;
+}
+.navbar_container {
+  text-align: center;
+  align-items: left;
+  display: flex;
+  width: 100%;
+  background-color: #ffffff00;
+  position: fixed;
+  z-index: 8;
+}
+.about_button {
+  display: flex;
+  align-self: right;
+}
+.button {
+  background-color: #e1e5ed00;
+
   border-radius: 15px;
-  box-sizing: border-box;
-  color: #3b3b3b;
+
+  color: #ffffff;
   cursor: pointer;
   display: inline-block;
   font-family: "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji",
@@ -57,29 +88,28 @@
   min-height: 60px;
   min-width: 0;
   outline: none;
-  padding: 16px 24px;
+
   text-align: center;
   text-decoration: none;
   transition: all 300ms cubic-bezier(0.23, 1, 0.32, 1);
-  user-select: none;
-  -webkit-user-select: none;
+
   touch-action: manipulation;
-  width: 30%;
+  width: fit-content;
   will-change: transform;
 }
 
-.button-1:disabled {
+.button:disabled {
   pointer-events: none;
 }
 
-.button-1:hover {
+.button:hover {
   color: #fff;
   background-color: #1a1a1a;
   box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
   transform: translateY(-2px);
 }
 
-.button-1:active {
+.button:active {
   box-shadow: none;
   transform: translateY(0);
 }
