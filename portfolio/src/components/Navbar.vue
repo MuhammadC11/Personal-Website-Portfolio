26f73569f026a3d1d528cac7fc5e54e65d02d3f2
Muhammad Chaudhry <chaudhrysayyam85@gmail.com>
2023-01-06 22:14:43 -0500

    fixed navbar

--- portfolio/src/components/Navbar.vue
+++ portfolio/src/components/Navbar.vue
@@ -10,6 +10,11 @@
         <button class="button1">Résumé</button>
       </router-link>
     </div>
+    <div>
+      <a target="_blank" class="gh_button" href="https://github.com/MuhammadC11"
+        >GitHub</a
+      >
+    </div>
   </div>
 </template>
 
@@ -19,6 +24,51 @@
 @import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css";
 @import "https://fonts.googleapis.com/css2?family=Quicksand:wght@600&display=swap";
 
+.home_button {
+  width: 100%;
+}
+.resume_button {
+  margin-left: 90rem;
+}
+.gh_button {
+  border: none;
+  outline: none;
+  padding: 0.5em 1.1em;
+  font-size: 1.2rem;
+  letter-spacing: 0.02em;
+  background-color: rgba(176, 255, 160, 0.233);
+  color: #fff;
+
+  cursor: pointer;
+  border-radius: 2.5px;
+  user-select: none;
+  transition: ease-in-out 0.15s all;
+}
+
+.gh_button:hover {
+  color: #000;
+}
+
+.gh_button:active {
+  animation: click 0.15s ease-in-out;
+}
+.gh_button {
+  font-size: 18px;
+  color: #ffffff;
+  font-family: inherit;
+  font-weight: 800;
+  cursor: pointer;
+  position: relative;
+  border: none;
+  background: none;
+  text-transform: uppercase;
+  transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+  transition-duration: 400ms;
+  transition-property: color;
+  text-decoration: none;
+  display: flex;
+  align-self: right;
+}
 .button1 {
   font-size: 18px;
   color: #ffffff;
@@ -32,6 +82,30 @@
   transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
   transition-duration: 400ms;
   transition-property: color;
+}
+.gh_button:focus,
+.gh_button:hover {
+  color: #fff;
+}
+
+.gh_button:focus:after,
+.gh_button:hover:after {
+  width: 100%;
+  left: 0%;
+}
+
+.gh_button:after {
+  content: "";
+  pointer-events: none;
+  bottom: -2px;
+  left: 50%;
+  position: absolute;
+  width: 0%;
+  height: 2px;
+  background-color: #fff;
+  transition-timing-function: cubic-bezier(0.25, 0.8, 0.25, 1);
+  transition-duration: 400ms;
+  transition-property: width, left;
 }
 
 .button1:focus,
@@ -94,7 +168,7 @@
   transition: all 300ms cubic-bezier(0.23, 1, 0.32, 1);
 
   touch-action: manipulation;
-  width: fit-content;
+  width: 100%;
   will-change: transform;
 }
 
